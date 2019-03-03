41bcaf877544c7ed3ffe8774bb32ae0517db1a33
Bonus <jiayilu96@gmail.com>
2019-03-02 23:44:34 -0500

    Added link to the budget page on home

--- budget/views.py
+++ budget/views.py
@@ -4,5 +4,4 @@ def project_list(request):
     return render(request, 'budget/project-list.html')
 
 def project_detail(request, project_slug):
-
     return render(request, 'budget/project-detail.html')
